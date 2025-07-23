6739fa304c844c807b1a6cf109090bacb94f3566
GitHub satomaruHUb <satoru-kubota@kccs.co.jp>
2025-07-23 17:12:08 +0900

    勤怠検索完了 新規ユーザー登録の枠まで完了

--- test/src/main/java/com/example/demo/controller/NewUserController.java
+++ test/src/main/java/com/example/demo/controller/NewUserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ public class NewUserController {
 		return "admin-menu-sample";
 	}
 	
-	@GetMapping("/new-user-request")
+	@GetMapping("/new-user-regist")
 	public String newUserRequest(
 			@ModelAttribute NewUserForm form) {
 		return "new-user-regist";
@@ -23,13 +24,12 @@ public class NewUserController {
 	
 	@PostMapping("/new-user-confirm")
 	public String newUserConfirm(
-			@ModelAttribute NewUserForm form) {
+			@ModelAttribute NewUserForm form, Model model) {
 		return "new-user-confirm";
 	}
 	
 	@GetMapping("/new-user-complete")
-	public String newUserComplete(
-			@ModelAttribute NewUserForm form) {
+	public String newUserComplete() {
 		return "new-user-complete";
 	}
 
